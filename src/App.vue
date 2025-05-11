d35a473dbd458e8f2ee8c8ad401db16be81a35ed
Jonathan Aspeling <jbaspeling@gmail.com>
2025-05-11 21:55:34 +0200

    Add layout structure with navigation and timer components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <script setup>
-import {default as PrimeButton} from "primevue/button"
+import { default as PrimeButton } from 'primevue/button';
 import DatePicker from 'primevue/datepicker';
-
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="flex flex-col h-screen">
     <!-- Blank canvas -->
-
+    <div
+      id="top-navbar"
+      class="flex border-2 p-2 text-2xl justify-center my-auto"
+    >
+      a
+    </div>
+    <div
+      id="main-timer"
+      class="flex border-2 p-2 text-2xl justify-center my-auto"
+    >
+      b
+    </div>
+    <div
+      id="round-indicator"
+      class="flex border-2 p-2 text-2xl justify-center my-auto"
+    >
+      c
+    </div>
+    <div
+      id="timer-controls"
+      class="flex border-2 p-2 text-2xl justify-center my-auto"
+    >
+      d
+    </div>
   </div>
 </template>
 
