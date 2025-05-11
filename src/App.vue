eaca2619ead878a1ea1a5621bd921a943cffaae6
Jonathan Aspeling <jbaspeling@gmail.com>
2025-05-11 22:17:59 +0200

    Working a bit more on the layout containers for the app

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,47 @@ import DatePicker from 'primevue/datepicker';
 </script>
 
 <template>
-  <div id="app" class="flex flex-col h-screen">
+  <div id="app" class="flex h-screen flex-col">
     <!-- Blank canvas -->
     <div
       id="top-navbar"
-      class="flex border-2 p-2 text-2xl justify-center my-auto"
+      class="flex flex-2/12 justify-center border-2 p-2 text-2xl"
     >
-      a
+      <div class="flex flex-4/12 justify-center">A</div>
+      <div class="flex flex-4/12 justify-center">B</div>
+      <div class="flex flex-4/12 justify-center">C</div>
     </div>
     <div
       id="main-timer"
-      class="flex border-2 p-2 text-2xl justify-center my-auto"
+      class="flex flex-10/12 justify-center border-2 p-2 text-2xl"
     >
-      b
+      <div class="m-auto flex justify-center text-8xl">00:00</div>
     </div>
     <div
       id="round-indicator"
-      class="flex border-2 p-2 text-2xl justify-center my-auto"
+      class="flex flex-10/12 justify-center border-2 p-2 text-2xl"
     >
-      c
+      <div class="m-auto flex-col">
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
+      </div>
     </div>
     <div
       id="timer-controls"
-      class="flex border-2 p-2 text-2xl justify-center my-auto"
+      class="flex flex-10/12 justify-center border-2 p-2 text-2xl"
     >
-      d
+      <div class="m-auto flex h-full w-full justify-center">
+        <div class="flex h-full w-full flex-col justify-center">
+          <div class="self-center">A</div>
+          <div class="self-center">B</div>
+          <div class="self-center">C</div>
+          <div class="self-center">D</div>
+        </div>
+        <div class="flex h-full w-full flex-col justify-center">
+          <div class="self-center">B</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
