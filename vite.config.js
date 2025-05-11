0f2d8c96eec22789557118afe090d81e47b38108
Jonathan Aspeling <jbaspeling@gmail.com>
2025-05-11 11:56:22 +0200

    Added tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
+  plugins: [vue(), tailwindcss(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
     scope: '/boxing-pwa/',
